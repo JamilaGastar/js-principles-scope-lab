d4c15901684dbb769532bef1f6379591893a1079
jamilagastar <jamilagastar@gmail.com>
2022-04-25 15:34:08 +1000

    paritally completed lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+console.log(customerName);
+
+function upperCaseCustomerName() {
+    console.log(customerName);
+    customerName = customerName.toUpperCase()
+    return customerName.toUpperCase(); 
+}
+
+const leastFavoriteCustomer = 'Karen';
+console.log(leastFavoriteCustomer);
+
+function changeLeastFavoriteCustomer() {
+    console.log(leastFavoriteCustomer);
+    leastFavoriteCustomer = 'George';
+    return leastFavoriteCustomer;
+}
+
+console.log (changeLeastFavoriteCustomer());
+
+
+
+
+    
+
