d953ffe237679bfc0b66e204e36d4604a79b6568
jamilagastar <jamilagastar@gmail.com>
2022-04-29 17:06:07 +1000

    completed lab

--- index.js
+++ index.js
@@ -1,40 +1,27 @@
 // Write your solution in this file!
 var customerName = 'bob';
-console.log(customerName);
+
 
 function upperCaseCustomerName() {
-    console.log(customerName);
     customerName = customerName.toUpperCase()
-    return customerName.toUpperCase(); 
 }
 
-bestCustomer;
-console.log(bestCustomer);
+var bestCustomer;
 
 function setBestCustomer() {
-    //bestCustomer = 'not bob';
-    return bestCustomer = 'not bob';
+    bestCustomer = 'not bob';
 }
-
-console.log(setBestCustomer());
 
 function overwriteBestCustomer() {
-    // bestCustomer = 'maybe bob';
-    return bestCustomer = 'maybe bob';
+    bestCustomer = 'maybe bob';
 }
-
-console.log(overwriteBestCustomer());
 
 const leastFavoriteCustomer = 'Karen';
-console.log(leastFavoriteCustomer);
 
 function changeLeastFavoriteCustomer() {
-    console.log(leastFavoriteCustomer);
     leastFavoriteCustomer = 'George';
-    return leastFavoriteCustomer;
 }
 
-console.log (changeLeastFavoriteCustomer());
 
 
     
